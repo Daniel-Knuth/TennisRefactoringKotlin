a9db4444314ac07808ffb1c34b8000a2131901fb
Knuth, Daniel (SE-A/36) <daniel.knuth1@volkswagen.de>
2024-05-23 09:54:00 +0200

    [WIP] refactoring

--- src/main/kotlin/TennisGame1.kt
+++ src/main/kotlin/TennisGame1.kt
@@ -14,22 +14,22 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         var score = ""
         var tempScore = 0
         if (isDraw()) {
-            when (scorePlayer1) {
-                0 -> score = "Love-All"
-                1 -> score = "Fifteen-All"
-                2 -> score = "Thirty-All"
-                else -> score = "Deuce"
+            score = when (scorePlayer1) {
+                0 -> "Love-All"
+                1 -> "Fifteen-All"
+                2 -> "Thirty-All"
+                else -> "Deuce"
             }
         } else if (scorePlayer1 >= 4 || scorePLayer2 >= 4) {
             val minusResult = scorePlayer1 - scorePLayer2
-            if (minusResult == 1)
-                score = "Advantage player1"
+            score = if (minusResult == 1)
+                "Advantage player1"
             else if (minusResult == -1)
-                score = "Advantage player2"
+                "Advantage player2"
             else if (minusResult >= 2)
-                score = "Win for player1"
+                "Win for player1"
             else
-                score = "Win for player2"
+                "Win for player2"
         } else {
             for (i in 1..2) {
                 if (i == 1)
