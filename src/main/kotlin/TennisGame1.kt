88ec573d58539d979434d3fa75ff10c117c78da6
Knuth, Daniel (SE-A/36) <daniel.knuth1@volkswagen.de>
2024-05-23 09:50:30 +0200

    [WIP] refactoring

--- src/main/kotlin/TennisGame1.kt
+++ src/main/kotlin/TennisGame1.kt
@@ -1,27 +1,27 @@
 class TennisGame1(private val player1Name: String, private val player2Name: String) : TennisGame {
 
-    private var m_score1: Int = 0
-    private var m_score2: Int = 0
+    private var scorePlayer1: Int = 0
+    private var scorePLayer2: Int = 0
 
     override fun wonPoint(playerName: String) {
-        if (playerName === "player1")
-            m_score1 += 1
-        else
-            m_score2 += 1
+        if (playerName == player1Name)
+            scorePlayer1 += 1
+        else if (playerName == player2Name)
+            scorePLayer2 += 1
     }
 
     override fun getScore(): String {
         var score = ""
         var tempScore = 0
-        if (m_score1 == m_score2) {
-            when (m_score1) {
+        if (scorePlayer1 == scorePLayer2) {
+            when (scorePlayer1) {
                 0 -> score = "Love-All"
                 1 -> score = "Fifteen-All"
                 2 -> score = "Thirty-All"
                 else -> score = "Deuce"
             }
-        } else if (m_score1 >= 4 || m_score2 >= 4) {
-            val minusResult = m_score1 - m_score2
+        } else if (scorePlayer1 >= 4 || scorePLayer2 >= 4) {
+            val minusResult = scorePlayer1 - scorePLayer2
             if (minusResult == 1)
                 score = "Advantage player1"
             else if (minusResult == -1)
@@ -33,10 +33,10 @@ class TennisGame1(private val player1Name: String, private val player2Name: Stri
         } else {
             for (i in 1..2) {
                 if (i == 1)
-                    tempScore = m_score1
+                    tempScore = scorePlayer1
                 else {
                     score += "-"
-                    tempScore = m_score2
+                    tempScore = scorePLayer2
                 }
                 when (tempScore) {
                     0 -> score += "Love"
